b1863a6c94a99b973ca77f33ec3e2185197c5b28
Amaranth-Development <amaranth.development@gmail.com>
2026-03-16 23:53:20 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'COVCHEG.UA',
-  description: 'First AI Helper',
+  description: 'First UA Refugees AI Helper',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" translate="no">
       <head>
         <meta name="google" content="notranslate" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <script src="https://telegram.org/js/telegram-web-app.js" />
       </head>
       <body className={inter.className}>{children}</body>
